59473e9bc404f59998a3d15b433137310e7b8f44
fahim ashhab <37286610+fahim-ash@users.noreply.github.com>
2021-06-17 10:20:36 +0600

    Add files via upload

---
+++ expression.java
@@ -0,0 +1,10 @@
+public class expression {
+
+ public static void main(String[] args) {
+  
+  // expression =     operands & operators
+  // operands =  values, variables, numbers, quantity
+  // operators = + - * / % 
+
+ } 
+}
---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+
+ public static void main(String[] args){
+  
+  int x = 123;
+  double y = 3.14;
+  boolean z = true;
+  char symbol = '@';
+  String name = "Ash";
+  
+  System.out.println("Hello "+name);
+  
+ }
+}
