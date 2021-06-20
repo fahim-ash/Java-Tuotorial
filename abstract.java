ca7774ab89cafe8bb18f2831b3e9149c22b185be
fahim ashhab <37286610+fahim-ash@users.noreply.github.com>
2021-06-20 21:15:34 +0600

    Add files via upload

---
+++ abstract.java
@@ -0,0 +1,29 @@
+//****************************************
+public class abstract {
+
+	public static void main(String[] args) {
+		
+		// abstract =  	abstract classes cannot be instantiated, but they can have a subclass
+		//				abstract methods are declared without an implementation
+		
+		//Vehicle vehicle = new Vehicle();
+		Car car = new Car();
+		
+		car.go();
+	}
+}
+//****************************************
+public abstract class Vehicle {
+	
+	abstract void go();
+}
+//****************************************
+public class Car extends Vehicle{
+
+	@Override
+	void go() {
+		System.out.println("The driver is driving the car");
+		
+	}
+}
+//****************************************
